baa4b68b8def2d3c9b2c8eca405f87a78c288c90
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-16 13:41:28 +0800

    Update install.php

--- install.php
+++ install.php
@@ -1,3 +1,8 @@
+<head>
+<meta http-equiv="refresh" content="0; url=/log.txt" />
+</head>
+<a href="/log.txt">redirect</a>
+
 <?php
-  exec('apt install $(cat "Aptfile") -y');
+  exec('apt install $(cat "Aptfile") -y >log.txt 2>&1 &');
 ?>
