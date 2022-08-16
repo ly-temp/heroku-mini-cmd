409325fd0214232a423ac0741b092bf81fe7f417
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-16 14:33:20 +0800

    Update get_my_web_bash.sh

--- bash/get_my_web_bash.sh
+++ bash/get_my_web_bash.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd /bin
+#cd /bin
 wget https://raw.githubusercontent.com/laiyuantemp/my_bash/main/crawler_LY.sh
 wget https://raw.githubusercontent.com/laiyuantemp/my_bash/main/downloader_LY.sh
 wget https://raw.githubusercontent.com/laiyuantemp/my_bash/main/scan_html_LY.sh
 wget https://raw.githubusercontent.com/laiyuantemp/my_bash/main/downloader_pausable_LY.sh
 wget https://raw.githubusercontent.com/laiyuantemp/my_bash/main/web3storage_LY.sh
 wget https://raw.githubusercontent.com/laiyuantemp/my_bash/main/web3storage_syncfolder_LY.sh
-chmod +x /bin/*.sh
+chmod +x ./*.sh
