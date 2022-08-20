f3670774a86449e8c8307e1fb149ae7baade66a4
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-08-20 09:37:09 +0800

    Update get_my_web_bash.sh

--- bash/get_my_web_bash.sh
+++ bash/get_my_web_bash.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #cd /bin
-wget https://raw.githubusercontent.com/laiyuantemp/my_bash/main/crawler_LY.sh
-wget https://raw.githubusercontent.com/laiyuantemp/my_bash/main/downloader_LY.sh
-wget https://raw.githubusercontent.com/laiyuantemp/my_bash/main/scan_html_LY.sh
-wget https://raw.githubusercontent.com/laiyuantemp/my_bash/main/downloader_pausable_LY.sh
-wget https://raw.githubusercontent.com/laiyuantemp/my_bash/main/web3storage_LY.sh
-wget https://raw.githubusercontent.com/laiyuantemp/my_bash/main/web3storage_syncfolder_LY.sh
-wget https://raw.githubusercontent.com/laiyuantemp/my_bash/main/ssh_ping_LY.sh
-wget https://raw.githubusercontent.com/laiyuantemp/my_bash/main/lock_crawler.sh
+wget https://raw.githubusercontent.com/ly-temp/my_bash/main/crawler_LY.sh
+wget https://raw.githubusercontent.com/ly-temp/my_bash/main/downloader_LY.sh
+wget https://raw.githubusercontent.com/ly-temp/my_bash/main/scan_html_LY.sh
+wget https://raw.githubusercontent.com/ly-temp/my_bash/main/downloader_pausable_LY.sh
+wget https://raw.githubusercontent.com/ly-temp/my_bash/main/web3storage_LY.sh
+wget https://raw.githubusercontent.com/ly-temp/my_bash/main/web3storage_syncfolder_LY.sh
+wget https://raw.githubusercontent.com/ly-temp/my_bash/main/ssh_ping_LY.sh
+wget https://raw.githubusercontent.com/ly-temp/my_bash/main/lock_crawler.sh
 
-wget https://github.com/laiyuantemp/static-binaries/raw/master/binaries/linux/x86_64/nano
-wget https://github.com/laiyuantemp/static-binaries/raw/master/binaries/linux/x86_64/socat
+wget https://github.com/ly-temp/static-binaries/raw/master/binaries/linux/x86_64/nano
+wget https://github.com/ly-temp/static-binaries/raw/master/binaries/linux/x86_64/socat
 chmod +x ./*
