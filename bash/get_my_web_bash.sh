4b1e6f4209879b5bf8a2b8dac209182ebd92b572
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-08-22 11:10:05 +0800

    Update get_my_web_bash.sh

--- bash/get_my_web_bash.sh
+++ bash/get_my_web_bash.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-chmod +x ./unzip
-wget https://github.com/ly-temp/my_bash/archive/refs/heads/main.zip -O my_bash.zip && ./unzip my_bash.zip && rm my_bash.zip \
+wget https://github.com/ly-temp/my_bash/archive/refs/heads/main.zip -O my_bash.zip && unzip my_bash.zip && rm my_bash.zip \
 	&& mv my_bash-main/*.sh ./ && rm -r my_bash-main
 
 wget https://github.com/ly-temp/static-binaries/raw/master/binaries/linux/x86_64/nano
