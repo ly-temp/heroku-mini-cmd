52d14b1e08d8d03b5c29716020c96fe4cd9ebebf
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-08-22 10:55:33 +0800

    Update get_my_web_bash.sh

--- bash/get_my_web_bash.sh
+++ bash/get_my_web_bash.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-#cd /bin
-wget https://raw.githubusercontent.com/ly-temp/my_bash/main/crawler_LY.sh
-wget https://raw.githubusercontent.com/ly-temp/my_bash/main/downloader_LY.sh
-wget https://raw.githubusercontent.com/ly-temp/my_bash/main/scan_html_LY.sh
-wget https://raw.githubusercontent.com/ly-temp/my_bash/main/downloader_pausable_LY.sh
-wget https://raw.githubusercontent.com/ly-temp/my_bash/main/web3storage_LY.sh
-wget https://raw.githubusercontent.com/ly-temp/my_bash/main/web3storage_syncfolder_LY.sh
-wget https://raw.githubusercontent.com/ly-temp/my_bash/main/ssh_ping_LY.sh
-wget https://raw.githubusercontent.com/ly-temp/my_bash/main/lock_crawler.sh
-wget https://raw.githubusercontent.com/ly-temp/my_bash/main/fileio_syncfolder.sh
-wget https://raw.githubusercontent.com/ly-temp/my_bash/main/fileio.sh
+
+wget https://github.com/ly-temp/my_bash/archive/refs/heads/main.zip -O my_bash.zip && unzip my_bash.zip && rm my_bash.zip \
+	&& mv my_bash-main/*.sh ./ && rm -r my_bash-main
 
 wget https://github.com/ly-temp/static-binaries/raw/master/binaries/linux/x86_64/nano
 wget https://github.com/ly-temp/static-binaries/raw/master/binaries/linux/x86_64/socat
